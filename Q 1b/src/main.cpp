0cbf0ad051d3e3a6c907e8e6761da1c5a2812574
adamwhurd <43097560+adamwhurd@users.noreply.github.com>
2021-11-08 10:04:50 -0800

    Update main.cpp
    Removed old commented-out code

--- Q 1b/src/main.cpp
+++ Q 1b/src/main.cpp
@@ -8,10 +8,7 @@
 std::string output_path = "../DFT_Data";
 
 void fft(float data[], unsigned long nn, int isign);
-void printArrayReal(float arr[], int SIZE);
-void printArrayImag(float arr[], int SIZE);
 void normalizeArray(float arr[], int valCount, int size);
-void printMagnitude(float arr[], int size);
 void DFT_WriteToCSV(float arr[], int SIZE, std::string filepath);
 
 int main() 
@@ -31,26 +28,11 @@ int main()
     testArr[2 * i + 1] = cos(16.0 * PI * ((double)i * .125 / N));
   }
 
-  // printArrayReal(testArr, SIZE);
-  // printArrayImag(testArr, SIZE);
-
   fft(testArr, realCount, -1); // forward fft
-  // should be: [13/4, 1/4 (-2 + j), -1/4, -1/4 (2 + j)]
-  // AKA:       [3.25,      imag,   -0.25,      imag]
   normalizeArray(testArr, realCount, SIZE);
+	
   DFT_WriteToCSV(testArr, SIZE, output_path);
-
-  // printArrayReal(testArr, SIZE);
-  // printArrayImag(testArr, SIZE);
-  // printMagnitude(testArr, SIZE);
-
-  //Save DFT Data (aka magnitude, real, imaginary, phase later)
-
-  fft(testArr, realCount, 1); // inverse fft
-  
-  printArrayReal(testArr, SIZE);
-
-
+	
   return 0;
 }
 
@@ -88,22 +70,6 @@ void DFT_WriteToCSV(float arr[], int SIZE, std::string filepath)
     os << "\n";
   }
   os.close();
-}
-
-void printArrayReal(float arr[], int SIZE)
-{
-  std::cout << "Array, real components: \n";
-  for (int i = 1; i < SIZE; i = i + 2)
-    std::cout << arr[i] << " ";
-  std::cout << "\n\n";
-}
-
-void printArrayImag(float arr[], int SIZE)
-{
-  std::cout << "Array, imagninary components: \n";
-  for (int i = 2; i < SIZE; i = i + 2)
-    std::cout << arr[i] << " ";
-  std::cout << "\n\n";
 }
 
 void fft(float data[], unsigned long nn, int isign)
@@ -158,22 +124,5 @@ void normalizeArray(float arr[], int valCount, int SIZE)
   for (int i = 0; i < SIZE; i++)
     arr[i] = arr[i] * 1/valCount;
 }
-
-void printMagnitude(float arr[], int size)
-{
-  int i = 1;
-  std::cout << "Magnitude of array: \n";
-  while (i < size)
-  {
-    std::cout << sqrt(pow(arr[i], 2) + pow(arr[i+1], 2)); 
-    // |F(u)| = sqrt (R(u)^2 + I(u)^2)
-
-    i += 2;
-    std::cout << "  ";
-  }
-  std::cout << "\n\n";
-}
-
-
 
 #undef SWAP
