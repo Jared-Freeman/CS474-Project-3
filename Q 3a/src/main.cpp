a2c1bbe11cb4a662894fabc0a4b4e83bfa95a8e0
Jared-Freeman <j99freeman@gmail.com>
2021-11-07 23:00:54 -0800

    Q2a,2b,2c. Added templates for Q3a and Q3b

--- Q 3a/src/main.cpp
+++ Q 3a/src/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char** argv)
   //Test using generated test image ////////////////////////////////////////////////
 
   const int TEST_SIZE = 512;
-  int whiteSquare = 32;
+  int whiteSquare = 128;
 
   
   double** testImage;
